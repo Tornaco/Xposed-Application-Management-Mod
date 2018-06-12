39bd2998b289e75266c6b3a5df39127826067783
Baozisoftware <hlj493595708@hotmail.com>
2018-06-12 10:20:21 +0800

    OPT make crash

--- x-apm-app/src/main/java/github/tornaco/xposedmoduletest/ui/tiles/app/MokeCrash.java
+++ x-apm-app/src/main/java/github/tornaco/xposedmoduletest/ui/tiles/app/MokeCrash.java
@@ -1,6 +1,8 @@
 package github.tornaco.xposedmoduletest.ui.tiles.app;
 
 import android.content.Context;
+import android.content.DialogInterface;
+import android.support.v7.app.AlertDialog;
 import android.view.View;
 
 import dev.nick.tiles.tile.QuickTile;
@@ -23,7 +25,16 @@ public class MokeCrash extends QuickTile {
             @Override
             public void onClick(View v) {
                 super.onClick(v);
-                throw new IllegalStateException("=== Crash test by user ===");
+                new AlertDialog.Builder(context)
+                        .setTitle(R.string.title_crash_moke)
+                        .setMessage(R.string.summary_crash_moke)
+                        .setCancelable(true)
+                        .setPositiveButton(context.getString(R.string.title_click_to_continue), new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int whichButton) {
+                                throw new IllegalStateException("=== Crash test by user ===");
+                            }
+                        })
+                        .show();
             }
 
             @Override
