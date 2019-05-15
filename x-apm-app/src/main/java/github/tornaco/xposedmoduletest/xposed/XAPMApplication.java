e7fdb34e3c0da0ca75f1155965fb6afaf7e3f112
yinheng <yinhengmail@163.com>
2019-05-15 18:19:34 +0800

    Code clean up

--- x-apm-app/src/main/java/github/tornaco/xposedmoduletest/xposed/XAPMApplication.java
+++ x-apm-app/src/main/java/github/tornaco/xposedmoduletest/xposed/XAPMApplication.java
@@ -3,7 +3,6 @@ package github.tornaco.xposedmoduletest.xposed;
 import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.content.Context;
-import android.preference.PreferenceManager;
 import android.support.multidex.MultiDex;
 import android.support.multidex.MultiDexApplication;
 import android.widget.Toast;
@@ -86,20 +85,11 @@ public class XAPMApplication extends MultiDexApplication {
                     @Override
                     public void onActivityResumed(Activity activity) {
                         super.onActivityResumed(activity);
-
                         // This is the run activity.
                         if (activity instanceof NavigatorActivityBottomNav) {
                             cacheRunningServices();
                             cacheInstalledApps();
                             AppSettings.cacheRecentTilesAsync(getApplicationContext());
-
-                            // Only test on debug build.
-                            if (BuildConfig.DEBUG && sGMSSupported) {
-                                // Start IntentService to register this application with GCM.
-//                                Intent intent = new Intent(activity.getApplicationContext(), XRegistrationIntentService.class);
-//                                startService(intent);
-                            }
-
                         }
                     }
                 });
@@ -114,28 +104,10 @@ public class XAPMApplication extends MultiDexApplication {
     }
 
     private void initLogger() {
-
-        // No need for play version, google has done this.
-        if (!isPlayVersion()) {
-//            CrashReport.initCrashReport(getApplicationContext(), "db5e3b88a3", BuildConfig.DEBUG);
-//            CrashReport.setIsDevelopmentDevice(getApplicationContext(), BuildConfig.DEBUG);
-//            // Set cache size.
-//            BuglyLog.setCache(12 * 1024);
-        }
-
         Logger.config(Settings.builder().tag("X-APM-C")
                 .logLevel(XSettings.isDevMode(this)
                         ? Logger.LogLevel.VERBOSE : Logger.LogLevel.WARN)
-                .logAdapter(new AndroidLogAdapter() {
-                    @Override
-                    public void e(String tag, String message) {
-                        super.e(tag, message);
-                        if (!isPlayVersion()) {
-                            // Report to bugly.
-                            // CrashReport.postCatchedException(new Throwable(message));
-                        }
-                    }
-                })
+                .logAdapter(new AndroidLogAdapter())
                 .build());
 
     }
@@ -146,8 +118,5 @@ public class XAPMApplication extends MultiDexApplication {
 
     private void forDev() {
         Toast.makeText(this, "Hello developer!", Toast.LENGTH_LONG).show();
-        PreferenceManager.getDefaultSharedPreferences(getApplicationContext())
-                .edit().putString("DEBUG_APP_START", String.valueOf(System.currentTimeMillis()))
-                .apply();
     }
 }
