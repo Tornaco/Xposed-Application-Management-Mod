8106d22a8e251936566330277b04fdcf030695f8
yinheng <yinhengmail@163.com>
2019-05-16 17:59:58 +0800

    Fix reference of BuildConfig in AppResource

--- x-apm-framework/src/main/java/github/tornaco/xposedmoduletest/framework/config/BuildConfig.java
+++ x-apm-framework/src/main/java/github/tornaco/xposedmoduletest/framework/config/BuildConfig.java
@@ -5,7 +5,7 @@ public final class BuildConfig {
     public static final String VERSION_NAME = github.tornaco.x.framework.BuildConfig.VERSION_NAME;
     public static final boolean DEBUG = github.tornaco.x.framework.BuildConfig.DEBUG;
     // Fields from build type: debug
-    public static final String X_APM_APP_PACKAGE_NAME = "github.tornaco.xposedmoduletest";
+    public static final String X_APM_APP_PACKAGE_NAME = github.tornaco.x.framework.BuildConfig.X_APM_APP_PACKAGE_NAME;
     public static final String X_APM_APP_PER_APP_SETTINGS_CLAZZ = "github.tornaco.xposedmoduletest.ui.activity.app.PerAppSettingsDashboardActivity";
     public static final String X_APM_APP_RUNNING_SERVICES_CLAZZ = "github.tornaco.xposedmoduletest.ui.activity.helper.RunningServicesActivity";
 }
